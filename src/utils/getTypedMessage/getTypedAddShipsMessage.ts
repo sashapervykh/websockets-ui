2bb79b9705bfa46fee7024d40db8b454def6b5bd
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-11-15 17:57:22 +0300

    feat: add type checking for ships property

--- src/utils/getTypedMessage/getTypedAddShipsMessage.ts
+++ src/utils/getTypedMessage/getTypedAddShipsMessage.ts
@@ -47,3 +47,48 @@ export function getTypedAddShipsMessage(message: MessageWithCheckedType) {
     id: message.id,
   };
 }
+
+function getTypedShips(ships: unknown) {
+  if (typeof ships !== "string") {
+    throw new Error(`Received ships is not string!`);
+  }
+  const shipsObj: unknown = JSON.parse(ships);
+  if (typeof shipsObj !== "object" || !shipsObj) {
+    throw new Error(`Ships are not valid JSON!`);
+  }
+  if (!Array.isArray(shipsObj)) {
+    throw new Error(`Ships are not an array!`);
+  }
+  const res = [];
+}
+
+function getTypedShip(ship: unknown) {
+  if (typeof ship !== "object" || !ship) {
+    throw new Error(`Ship is not valid JSON!`);
+  }
+  if (!("direction" in ship && typeof ship["direction"] === "boolean"))
+    throw new Error("Direction property of some ships is not boolean");
+  if (!("length" in ship && typeof ship["direction"] === "number"))
+    throw new Error("Length property of some ships is not a number");
+  if (!("type" in ship && typeof ship["type"] === "string"))
+    throw new Error("Type property of some ships is not string");
+  if (
+    !(
+      "position" in ship &&
+      typeof ship["position"] === "object" &&
+      ship["position"] &&
+      "x" in ship["position"] &&
+      "y" in ship["position"] &&
+      typeof ship["position"]["x"] === "number" &&
+      typeof ship["position"]["y"] === "number"
+    )
+  )
+    throw new Error("Position property of some ships is not a valid object");
+
+  return {
+    direction: ship.direction,
+    length: ship.length,
+    type: ship.type,
+    position: { x: ship.position.x, y: ship.position.y },
+  };
+}
