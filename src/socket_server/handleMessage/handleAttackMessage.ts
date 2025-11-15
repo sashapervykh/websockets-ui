cf8a4ba26128b411ae8b4cb86d11ff841c93dd07
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-11-15 21:56:14 +0300

    feat: send attack message

--- src/socket_server/handleMessage/handleAttackMessage.ts
+++ src/socket_server/handleMessage/handleAttackMessage.ts
@@ -5,6 +5,7 @@ import type {
 } from "../../model/message.js";
 import { getTypedAttackMessage } from "../../utils/getTypedMessage/getTypedAttackMessage.js";
 import { database } from "../../db/database.js";
+import { sendAttackMessage } from "../../utils/sendMessage/sendAttackMessage.js";
 
 export function handleAttackMessage(
   message: MessageWithCheckedType,
@@ -13,19 +14,24 @@ export function handleAttackMessage(
   const { x, y, gameId, indexPlayer } = getTypedAttackMessage(message).data;
   const game = database.getGame(gameId);
   if (!game) return;
-  const user = game.filter((elem) => elem.index === indexPlayer)[0];
+  const user = game.filter((elem) => elem.index !== indexPlayer)[0];
   if (!user) return;
+
+  const attackData = {
+    position: { x, y },
+    currentPlayer: indexPlayer,
+    status: "miss",
+  };
   for (const ship of user.shipsStored) {
     const isShot = checkShot(x, y, ship);
     if (isShot) {
       ship.shot++;
       ship.killed = ship.length - ship.shot === 0 ? true : false;
+      attackData.status = ship.killed ? "killed" : "shot";
       break;
     }
   }
-
-  if (user.shipsStored.every((elem) => elem.killed)) {
-  }
+  sendAttackMessage(gameId, attackData);
 }
 
 function checkShot(x: number, y: number, ship: ShipStored) {
